3cd467123aa33c2ee936ea3585a94c5e1b8b9011
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-03 17:53:07 +0900

    ビュー作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_11_30_063701) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "userame", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
