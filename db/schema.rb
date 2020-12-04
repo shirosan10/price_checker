b30d546cccad1bf30f5b3cd1cca1f2db052fd79b
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-04 23:12:03 +0900

    itemsテーブル作成２

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,17 @@
 ActiveRecord::Schema.define(version: 2020_12_03_144436) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "note", null: false
+    t.integer "jan_code", null: false
+    t.string "model_num", null: false
+    t.integer "store_name", null: false
+    t.date "daytime", null: false
+    t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -30,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_144436) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
