1ea54859fdc7006345cee90f9aa48656d9401507
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-05 11:02:11 +0900

    itemsバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+    validates :user,           presence: true
+    validates :title,           presence: true
+    validates :note,           presence: true
+    validates :jan_code,           presence: true
+    validates :model_num,           presence: true
+    validates :store_name,           presence: true
+    validates :daytime,           presence: true
+    validates :price,           presence: true
 end
