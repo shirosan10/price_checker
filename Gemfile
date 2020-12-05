c05fe0ec0c4b6990b6a924c165e18c0a2771c3aa
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-05 15:25:23 +0900

    ActiveStorage導入

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'pry-rails' 
+  gem 'mini_magick'
+  gem 'image_processing', '~> 1.2'
 end
 
 group :development do
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attaced :image
 
-    validates :user,           presence: true
-    validates :title,           presence: true
-    validates :note,           presence: true
-    validates :jan_code,           presence: true
-    validates :model_num,           presence: true
-    validates :store_name,           presence: true
-    validates :daytime,           presence: true
-    validates :price,           presence: true
+  validates :user,           presence: true
+  validates :title,           presence: true
+  validates :note,           presence: true
+  validates :jan_code,           presence: true
+  validates :model_num,           presence: true
+  validates :store_name,           presence: true
+  validates :daytime,           presence: true
+  validates :price,           presence: true
+  
 end
