9ca3ab4f7bd8c655391cea79be33f3659584254b
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-05 16:06:43 +0900

    storeテーブル作成

--- db/migrate/20201203144436_create_items.rb
+++ db/migrate/20201203144436_create_items.rb
@@ -4,11 +4,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
 
       t.references :user,                null: false, foreign_key: true
       t.string  :title,                  null: false
-      t.text    :note,             null: false
+      t.text    :note,                   null: false
       t.integer :jan_code,               null: false
       t.string  :model_num,              null: false
-      t.integer :store_name,        null: false
-      t.date :daytime,          null: false
+      t.integer :store_name,             null: false
+      t.date :daytime,                   null: false
       t.integer :price,                  null: false
       t.timestamps
     end
--- db/migrate/20201205065758_create_stores.rb
+++ db/migrate/20201205065758_create_stores.rb
@@ -2,6 +2,14 @@ class CreateStores < ActiveRecord::Migration[6.0]
   def change
     create_table :stores do |t|
 
+      t.string :store_name,		    null: false
+      t.integer	:phone,	
+      t.string	:station,	        null: false
+      t.string	:zip_code,	      null: false
+      t.integer :prefecture_id,		null: false
+      t.integer :city_id,		      null: false
+      t.string	:line,          	null: false
+
       t.timestamps
     end
   end
