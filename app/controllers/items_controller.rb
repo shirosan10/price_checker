3159ac082af4bd356db3d7f643ba3520ed520561
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-05 15:35:02 +0900

    Active Storage導入に伴うコントローラーの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,25 @@
 class ItemsController < ApplicationController
-  def index
-  
-  end
-
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create params.require(:item).permit(:content, :image) # POINT
+    redirect_to @item
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id]) 
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update params.require(:item).permit(:image,) # POINT
+    redirect_to @item
   end
 
 end
