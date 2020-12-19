84a52c93550e4bf9e5039e93f2a7df2ed6916e69
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-12-19 10:15:31 +0900

    ビューの追加

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,13 @@ module PriceChecker
     # the framework and any gems in your application.
   end
 end
+
+module TimeFormatSandbox
+  class Application < Rails::Application
+
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
+    # デフォルトのロケールを日本（ja）に設定
+    config.i18n.default_locale = :ja
+  end
+end
